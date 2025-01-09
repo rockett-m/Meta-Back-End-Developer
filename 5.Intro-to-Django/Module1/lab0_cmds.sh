2d31fe0034c0d8719d06330db433086235a2cdc6
Morgan Rockett <morgan.rockett@tufts.edu>
2025-01-08 20:48:24 -0500

    Part 5/9 Intro to Django; 1/5 modules done

---
+++ 5.Intro-to-Django/Module1/lab0_cmds.sh
@@ -0,0 +1,16 @@
+
+/Users/sudo/CodeProjects/Meta-Back-End-Developer/5.Intro-to-Django/Module1/
+django-admin startproject myproject
+
+cd myproject
+
+python3 manage.py startapp myapp
+
+python3 manage.py runserver
+
+# browser
+http://127.0.0.1:8000/
+
+http://127.0.0.1:8000/admin
+# becomes login page:
+http://127.0.0.1:8000/admin/login/?next=/admin/
